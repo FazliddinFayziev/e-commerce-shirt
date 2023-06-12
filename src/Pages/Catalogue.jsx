1f1b0dcf25cb500b1d19595631575b6d498d618f
FazliddinFayziev <fazrez4515@gmail.com>
2023-06-12 23:36:36 +0800

    r

--- src/Pages/Catalogue.jsx
+++ src/Pages/Catalogue.jsx
@@ -1,31 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TiThMenu } from "react-icons/ti";
+import { AiOutlineClose, AiOutlineShopping, AiOutlineSearch } from "react-icons/ai";
 
-
-// Styles
 import "../Style/catalogue.css";
 
 const Catalogue = () => {
-    return (
-        <>
-            <div className='both-catalogue'>
-                {/* <div className='filter'></div> */}
-                <div className='catalogue'>
-                    <div className='above-nav-container'>
-                        <nav className='above-nav'>
-                            <div onClick={() => { '' }} className='nav'>
-                                <TiThMenu fontSize={20} />
-                            </div>
+    const [sidebar, setSidebar] = useState(false);
 
-                            <ul>
-                                <p></p>
-                            </ul>
-                        </nav>
+    return (
+        <div className='both-catalogue'>
+            <div className={`filter ${!sidebar ? 'close' : ''}`}>
+                <div className='close-filter'>
+                    <AiOutlineClose onClick={() => setSidebar(false)} className='close-icon' fontSize={25} />
+                </div>
+            </div>
+            <div className='catalogue'>
+                <div className='above-nav-container'>
+
+                    {/* FIRST CATALOGUE */}
+
+                    <nav className='above-nav'>
+                        <div onClick={() => setSidebar(true)} className='nav-filter'>
+                            <TiThMenu fontSize={20} />
+                        </div>
+                        <div className='big-screen-nothing'></div>
+
+                        <ul className='language'>
+                            <p className='active'>Eng</p>
+                            <p className='no-active'>Ru</p>
+                            <p className='no-active'>Uz</p>
+                        </ul>
+                    </nav>
+
+                    {/* SECOND CATALOGUE */}
+
+                    <div className='home-catalogue-nav'>
+
+                        <div className='catalogue-one'>
+                            <p className='no-active'>Home</p>
+                            <div className='catalogue-dot'>
+                                <p className='active'>Catalogue</p>
+                                <div className='dot'></div>
+                            </div>
+                        </div>
+
+                        <div className='catalogue-two'>
+                            <div className='catalogue-search'>
+                                <AiOutlineSearch fontSize={30} />
+                            </div>
+                            <div className='catalogue-box'>
+                                <AiOutlineShopping fontSize={30} />
+                            </div>
+                            <div className='catalogue-box-count-product'><p>3</p></div>
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </>
-    )
-}
+        </div>
+    );
+};
 
-export default Catalogue
+export default Catalogue;
