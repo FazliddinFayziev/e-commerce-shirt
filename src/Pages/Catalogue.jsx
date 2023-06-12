76653c2e689a6f87522ebb3fc29d6cac2f43141f
FazliddinFayziev <fazrez4515@gmail.com>
2023-06-13 00:43:08 +0800

    t

--- src/Pages/Catalogue.jsx
+++ src/Pages/Catalogue.jsx
@@ -3,26 +3,42 @@ import { TiThMenu } from "react-icons/ti";
 import { AiOutlineClose, AiOutlineShopping, AiOutlineSearch } from "react-icons/ai";
 
 import "../Style/catalogue.css";
+import { popular } from '../Data/data';
 
 const Catalogue = () => {
     const [sidebar, setSidebar] = useState(false);
 
     return (
         <div className='both-catalogue'>
+
             <div className={`filter ${!sidebar ? 'close' : ''}`}>
+
                 <div className='close-filter'>
+
                     <AiOutlineClose onClick={() => setSidebar(false)} className='close-icon' fontSize={25} />
+
                 </div>
+
+                <div className='main-filter-container'></div>
+
             </div>
+
+
+
             <div className='catalogue'>
+
                 <div className='above-nav-container'>
 
                     {/* FIRST CATALOGUE */}
 
                     <nav className='above-nav'>
+
+                        {/* MENU  */}
+
                         <div onClick={() => setSidebar(true)} className='nav-filter'>
                             <TiThMenu fontSize={20} />
                         </div>
+
                         <div className='big-screen-nothing'></div>
 
                         <ul className='language'>
@@ -55,9 +71,35 @@ const Catalogue = () => {
                         </div>
 
                     </div>
+
+                    {/* All clothes */}
+
+                    <div className='all-clothes'>
+                        {popular.map((product) => {
+                            return (
+                                <div>
+                                    <div className='single-cloth'>
+                                        <img src={product.img} alt="single-cloth" />
+                                    </div>
+
+                                    <div className='shirt-container'>
+                                        <div className='shirt-info'>
+                                            <p className='category'>category</p>
+                                            <p className='title'>our title</p>
+                                            <p className='price'>20 $</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+
                 </div>
+
             </div>
+
         </div>
+
     );
 };
 
